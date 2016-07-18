a0984833450b0b60de633ef5812f39e42dab4463
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2016-07-18 23:45:01 +0900

    Update gravitar hash tag and fixed phone number.

--- _build.sh
+++ _build.sh
@@ -7,5 +7,5 @@ jekyll build
 # Remove the App Engine config file (see step #6)
 # rm _site/app.yaml
 
-appcfg.py --oauth2 --noauth_local_webserver -A nangrang-1366 -V v1 update ./_site
-#/Applications/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine/appcfg.py --oauth2 --noauth_local_webserver -A nangrang-1366 -V v1 update ./_site
+#appcfg.py --oauth2 --noauth_local_webserver -A nangrang-1366 -V v1 update ./_site
+/Applications/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine/appcfg.py --oauth2 --noauth_local_webserver -A nangrang-1366 -V v1 update ./_site
